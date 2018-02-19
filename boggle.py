1944d3c9381160f4483df40bcf10bd49bc5dea2d
Liam Cafearo <lcafearo@gmail.com>
2018-02-19 21:29:11 +0000

    added empty grid test

--- boggle.py
+++ boggle.py
@@ -1,2 +1,7 @@
 def check():
     return 1
+
+
+def make_grid(width, height):
+    return {}
+
--- test_boggle.py
+++ test_boggle.py
@@ -4,3 +4,7 @@ import boggle
 class TestBoggle(unittest.TestCase):
     def test_Is_This_Thing_On(self):
         self.assertEqual(1, boggle.check())
+
+    def test_can_create_an_empty_grid(self):
+        grid = boggle.make_grid(0, 0)
+        self.assertEqual(len(grid), 0)
