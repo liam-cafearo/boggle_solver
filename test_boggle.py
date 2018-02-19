9f0cd683b998217b706311d1ef655817d1e5bc38
Liam Cafearo <lcafearo@gmail.com>
2018-02-19 21:53:42 +0000

    added coordinates

--- test_boggle.py
+++ test_boggle.py
@@ -12,3 +12,11 @@ class TestBoggle(unittest.TestCase):
     def test_grid_size_is_width_times_height(self):
         grid = boggle.make_grid(2, 3)
         self.assertEqual(len(grid), 6)
+
+    def test_grid_coordinates(self):
+        grid = boggle.make_grid(2, 2)
+        self.assertTrue((0, 0) in grid)
+        self.assertTrue((0, 1) in grid)
+        self.assertTrue((1, 0) in grid)
+        self.assertTrue((1, 1) in grid)
+        self.assertTrue((2, 2) not in grid)
