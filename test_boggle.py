9b893e1eb2b6d2c5e1f4ab934455f6d94127c45b
Liam Cafearo <lcafearo@gmail.com>
2018-02-20 21:18:01 +0000

    added neighbours functionality

--- test_boggle.py
+++ test_boggle.py
@@ -27,3 +27,23 @@ class TestBoggle(unittest.TestCase):
         grid = boggle.make_grid(2, 3)
         for L in grid.values():
             self.assertTrue(L in ascii_uppercase)
+
+    def test_neighbours_of_a_position(self):
+        neighbours = boggle.neighbours_of_position((1, 2))
+        self.assertTrue((0, 1) in neighbours)
+        self.assertTrue((0, 2) in neighbours)
+        self.assertTrue((0, 3) in neighbours)
+        self.assertTrue((1, 1) in neighbours)
+        self.assertTrue((1, 3) in neighbours)
+        self.assertTrue((2, 1) in neighbours)
+        self.assertTrue((2, 2) in neighbours)
+        self.assertTrue((2, 3) in neighbours)
+
+    def test_all_grid_neighbours(self):
+        grid = boggle.make_grid(2, 2)
+        neighbours = boggle.all_grid_neighbours(grid)
+        self.assertEqual(len(neighbours), len(grid))
+        for pos in grid:
+            others = list(grid) #creates a new list from the dictionary's keys
+            others.remove(pos)
+            self.assertListEqual(sorted(neighbours[pos]), sorted(others))
