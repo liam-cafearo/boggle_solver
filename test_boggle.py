edb91ed5859bc1634dfb1f421c6e4d6e706eb531
Liam Cafearo <lcafearo@gmail.com>
2018-03-06 21:24:47 +0000

    added finding words functionality

--- test_boggle.py
+++ test_boggle.py
@@ -44,6 +44,32 @@ class TestBoggle(unittest.TestCase):
         neighbours = boggle.all_grid_neighbours(grid)
         self.assertEqual(len(neighbours), len(grid))
         for pos in grid:
-            others = list(grid) #creates a new list from the dictionary's keys
+            # creates a new list from the dictionary's keys
+            others = list(grid)
             others.remove(pos)
             self.assertListEqual(sorted(neighbours[pos]), sorted(others))
+
+    def test_converting_a_path_to_a_word(self):
+        grid = boggle.make_grid(2, 2)
+        oneLetterWord = boggle.path_to_word(grid, [(0, 0)])
+        twoLetterWord = boggle.path_to_word(grid, [(0, 0), (1, 1)])
+        self.assertEqual(oneLetterWord, grid[(0, 0)])
+        self.assertEqual(twoLetterWord, grid[(0, 0)] + grid[(1, 1)])
+
+    def test_search_grid_for_words(self):
+        grid = {(0, 0): 'A', (0, 1): 'B', (1, 0): 'C', (1, 1): 'D'}
+        twoLetterWord = 'AB'
+        threeLetterWord = 'ABC'
+        notThereWord = "EEE"
+        dictionary = [twoLetterWord, threeLetterWord, notThereWord]
+
+        foundWords = boggle.search(grid, dictionary)
+
+        self.assertTrue(twoLetterWord in foundWords)
+        self.assertTrue(threeLetterWord in foundWords)
+        self.assertTrue(notThereWord not in foundWords)
+
+    def test_load_dictionary(self):
+        dictionary = boggle.get_dictionary('C:\\Users\\liam_\\PycharmProjects\\boggle_solver\\words.txt')
+        self.assertGreater(len(dictionary), 0)
+
